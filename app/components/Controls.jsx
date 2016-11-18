953c49bcf0ebcc1b178c8f354d3eb371976c4bad
Tyler Collins <tjscollins@gmail.com>
2016-11-18 22:40:49 +1000

    Added basic styles for Controls and Info components

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var Controls = React.createClass({
   render: function() {
     return (
-      <div>
-        <h1>Controls.jsx</h1>
+      <div className="row">
+        <div className="controls-box col-xs-10 col-xs-push-1">
+          <h1>Controls.jsx</h1>
+        </div>
       </div>
     );
   }
--- app/components/Info.jsx
+++ app/components/Info.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Info = (props) => {
   return (
-    <div>
+    <div className="info-box">
       <h1>Info.jsx</h1>
     </div>
   );
